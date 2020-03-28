4a4684af464bd0edf2be47c1d0ca1b1d5ec5f225
jpreciadom <36993234+jpreciadom@users.noreply.github.com>
2020-03-28 11:08:58 -0500

    Estructuras

--- Tarea 01/Ejercicio 1.6/main.c
+++ Tarea 01/Ejercicio 1.6/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+#define NUMMAQUINAS 3;
+
+struct Maquina;
+struct Pieza;
+
+struct Maquina {
+    short Id;                                   //Numero de la máquina
+    short EstadoDeActividad;                    //Si esta ocupada o disponible
+    long TiempoDeUso;                           //Tiempo total en segundos que ha sido usada la máquina
+    long TiempoDeInactividad;                   //Tiempo que la maquina estuvo desocupada
+    int PiezasProducidas;                       //Cantidad de piezas que ha producido la máquina en lo que lleva de uso
+    struct Pieza *TrabajandoEn;                 //Indica la pieza en la que esta trabajando la maquina
+};
+
+struct Pieza {
+    int Id;                                     //Identificador unico de la pieza
+    int TiempoDeLlegada[NUMMAQUINAS];                     //Momento en el que la pieza llego a la maquina
+    long Tiempoesperado[NUMMAQUINAS];
+    struct Maquina *AtendidoPor;                //Indica la máquina que la esta atendiendo
+};
+
+int main() {
+
     return 0;
 }
