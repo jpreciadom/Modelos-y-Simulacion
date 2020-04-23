16dba255628cd2f5bbbf96517fbbe185ba0fe450
jpreciadom <36993234+jpreciadom@users.noreply.github.com>
2020-04-23 13:58:54 -0500

    Ejercicio 2.8 terminado

--- Tarea 02/Ejercicio_1.2/main.c
+++ Tarea 02/Ejercicio_1.2/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "simlib.h"
+#include "Lector.h"
+
+//Eventos
+#define LLEGADA_DE_UNA_PERSONA                  1
+#define CUMPLIMIENTO_DE_TIEMPO_DE_ESPERA        2
+#define FIN_DEL_SERVICIO                        3
+//Listas
+#define COLA                                    1
 
 int main() {
     printf("Hello world!\n");
